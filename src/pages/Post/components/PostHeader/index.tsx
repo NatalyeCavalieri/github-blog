a092122395d59de71451544eac449179b060c97e
Natalye Cavalieri <cavalieridev@gmail.com>
2024-05-16 08:52:49 +0200

    add new pages and components and integration with github api

---
+++ src/pages/Post/components/PostHeader/index.tsx
@@ -0,0 +1,43 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { ExternalLink } from "../../../../components/externalLinks";
+import { PostHeaderContainer } from "./styles";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faCalendar, faChevronLeft, faComment } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
+
+export function PostHeader() {
+    const navigate = useNavigate()
+
+    function goBack(){
+        navigate(-1)
+    }
+  return (
+    <PostHeaderContainer>
+      <header>
+        <ExternalLink 
+        as="button" 
+        onClick={goBack}
+        icon={<FontAwesomeIcon icon={faChevronLeft}/>} 
+        text="Voltar"
+        variant="iconLeft" />
+        <ExternalLink text="Ver no github" href="#" target="_blank" />
+      </header>
+      <h1>JavaScript data types and data structures</h1>
+      <ul>
+        <li>
+          <FontAwesomeIcon icon={faGithub} />
+          cameronwll
+        </li>
+        <li>
+          <FontAwesomeIcon icon={faCalendar} />
+          Há 1 dia
+        </li>
+
+        <li>
+          <FontAwesomeIcon icon={faComment} />
+          5 comentários
+        </li>
+      </ul>
+    </PostHeaderContainer>
+  );
+}
